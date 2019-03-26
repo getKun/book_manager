a4e4ea440133fa077e6a40650efdad77fc0b51c4
Kun <jnrkun@163.com>
2019-03-26 16:11:51 +0800

    新增登录功能

--- src/com/cduest/controller/LoginServlet.java
+++ src/com/cduest/controller/LoginServlet.java
@@ -6,6 +6,11 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.cduest.model.User;
+import com.cduest.service.IUserService;
+import com.cduest.service.impl.UserSer;
 
 /**
  * Servlet implementation class LoginServlet
@@ -25,6 +30,25 @@ public class LoginServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		/*
+		 * 登录成功后，session中存放user对象
+		 */
+		String uid=request.getParameter("uid");
+		String pwd=request.getParameter("pwd");
+		User user=new User(uid, pwd);
+		IUserService login=new UserSer();
+		boolean b=login.login(user);
+		//b为true时，登录成功
+		if(b) {
+			
+			HttpSession session=request.getSession();
+			session.setAttribute("user", user);
+			request.getRequestDispatcher("main_page.jsp").forward(request, response);
+			
+		}else {
+			//登录失败，刷新当前页面
+			request.getRequestDispatcher("index.jsp").forward(request, response);
+		}
 		
 		
 	}
--- src/com/cduest/service/impl/UserSer.java
+++ src/com/cduest/service/impl/UserSer.java
@@ -1,11 +1,58 @@
 package com.cduest.service.impl;
 
+import com.cduest.dao.IUserLoginAndRegister;
+import com.cduest.dao.impl.UserLoginAndRegisterJdbc;
+import com.cduest.model.Book;
+import com.cduest.model.User;
+import com.cduest.service.IUserService;
 
 /**
  * 用户service层实现类
  * @author 1630720115
  *
  */
-public class UserSer {
+public class UserSer implements IUserService{
+	
+	User user=new User();
+
+	@Override
+	public boolean login(User user) {
+
+		String uid=user.getUid();
+		String pwd=user.getPwd();
+		//如果用户登录时输入的账号密码不为空时，调用dao层登录接口查看数据库中账户密码是否正确
+		if(!uid.equals(null)&&!pwd.equals(null)) {
+			IUserLoginAndRegister login=new UserLoginAndRegisterJdbc();
+			boolean b=login.login(user);
+			//返回值为true或false
+			return b;
+		}
+		//账号密码为空，直接返回false
+		return false;
+	}
+
+	@Override
+	public boolean register(User user) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean registerJudge(User user) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean borrowBook(User user, Book book) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean returnBook(User user, Book book) {
+		// TODO Auto-generated method stub
+		return false;
+	}
 
 }
